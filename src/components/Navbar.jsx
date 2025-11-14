f965e9480d75c147c0393e11b68f0be0a4cea455
Aditya Patil <adityas_mac@Adityas-MacBook-Air.local>
2025-11-14 14:48:59 +0530

    landing page modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
       <ul>
         <li><a href="#">home</a></li>
         <li><a href="https://drive.google.com/file/d/1V1UdJF6jzxoka0zY-rQbQXaepCblkUxD/view?usp=sharing">resume</a></li>
-        <li><a href="projects.html">projects</a></li>
+        <li><a href="#">projects</a></li>
       </ul>
     </nav>
   );
