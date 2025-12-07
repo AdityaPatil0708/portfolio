11db66c53cb7d0d01bcdfdfed8cfb1ff313af7e1
Aditya Patil <adityas_mac@Adityas-MacBook-Air.local>
2025-12-08 00:07:10 +0530

    changed profile image format

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import profile from '../images/adityaprofile2.webp';
+import profile from '/images/adityaprofile2.webp';
 
 export default function Profile() {
     return (
@@ -11,7 +11,7 @@ export default function Profile() {
                 <h2>Skills</h2>
                 <p> <strong>• Languages :</strong> C++, HTML, CSS, JavaScript</p>
                 <p> <strong>• Technologies :</strong> Express.js, Node.js, Websockets, MongoDB, PostgreSQL </p>
-                <p> <strong>• DevOps and Tools :</strong> Docker, Kubernetes, AWS, GitHub, Jenkins, GitHub Actions, Prometheus </p>
+                <p> <strong>• DevOps and Tools :</strong> Docker, Kubernetes, AWS, GitHub, Jenkins, GitHub Actions </p>
                 <p> <strong>• General:</strong> Data Structures, Algorithms, Database Management System, Computer Networks.</p>
             </div>
 
