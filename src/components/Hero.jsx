3a945bb9f43b46baa1f8b5a9e734242829054bde
Aditya Patil <adityas_mac@Adityas-MacBook-Air.local>
2025-12-10 13:13:53 +0530

    changed role

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
         <section className="hero">
             <h1>Aditya Patil</h1>
             <p>
-                I'm open to <strong> Full Stack Developer & DevOps Engineer </strong>roles with a strong focus on building scalable server-side systems and creating automated, reliable deployment workflows. Through internships, projects, and continuous learning, I've gained expertise in backend development, API design, database management, cloud services, and infrastructure automation. I specialize in developing high-performance RESTful APIs, containerizing applications, and implementing CI/CD pipelines using tools like Docker, Kubernetes, and Jenkins. With experience across Node.js, Express.js, SQL/NoSQL databases, and AWS, I enjoy architecting systems that are efficient, secure, and easy to maintain. 
+                I'm a<strong> Full Stack Developer & DevOps Engineer </strong> with a strong focus on building scalable server-side systems and creating automated, reliable deployment workflows. Through internships, projects, and continuous learning, I've gained expertise in backend development, API design, database management, cloud services, and infrastructure automation. I specialize in developing high-performance RESTful APIs, containerizing applications, and implementing CI/CD pipelines using tools like Docker, Kubernetes, and Jenkins. With experience across Node.js, Express.js, SQL/NoSQL databases, and AWS, I enjoy architecting systems that are efficient, secure, and easy to maintain. 
             </p>
 
         </section>
