4792057bb4487c9f0168cb3986a7e547eca8248f
Aditya Patil <adityas_mac@Adityas-MacBook-Air.local>
2025-12-14 21:56:05 +0530

    added project link

--- src/components/HeroW.jsx
+++ src/components/HeroW.jsx
@@ -1,38 +1,50 @@
 export default function Hero2() {
-    return (
-        <section className="hero2">
-            <div className="title">
-                <h2>Projects</h2>
-            </div>
-            <div className="project1">
-                <div className="project1-title">
-                    <div>
-                     <h3>Chat-App (WebSockets)</h3>
-                    </div>
-                    
-                    <button>
-                        <a href="https://github.com/AdityaPatil0708/chat-app-websockets-.git"><i class="fab fa-github"></i></a>
-                    </button>
-                </div>
-            </div>
-            <div className="project2">
-                <div className="project2-title">
-                    <h3>Sneaker-Store</h3>
-                    <button>
-                        <a href="https://github.com/AdityaPatil0708/UrbanSteps.git"><i class="fab fa-github"></i></a>
-                    </button>
-                </div>
-            </div>
-            <div className="project3">
-                <div className="project3-title">
-                    <h3>Rest API Service (CI/CD)</h3>
-                    <button>
-                        <a href="https://github.com/AdityaPatil0708/rest-api-service.git"><i class="fab fa-github"></i></a>
-                    </button>
-                </div>
-            </div>
-            <br />
-        </section>
-        
-    );
+  return (
+    <section className="hero2">
+      <div className="title">
+        <h2>Projects</h2>
+      </div>
+      <div className="project1">
+        <div className="project1-title">
+          <div>
+            <h3>Chat-App (WebSockets)</h3>
+          </div>
+
+          <button>
+            <a href="https://github.com/AdityaPatil0708/chat-app-websockets-.git">
+              <i class="fab fa-github"></i>
+            </a>
+          </button>
+        </div>
+      </div>
+      <div className="project2">
+        <div className="project2-title">
+          <h3>Sneaker-Store (*work in progress)</h3>
+          <div>
+            <button>
+              <a href="https://github.com/AdityaPatil0708/UrbanSteps.git">
+                <i class="fab fa-github"></i>
+              </a>
+            </button>
+            <button>
+              <a href="https://sneaker-store-frontend-xi.vercel.app/">
+                <i class="link-icon">↗</i>
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="project3">
+        <div className="project3-title">
+          <h3>Rest API Service (CI/CD)</h3>
+          <button>
+            <a href="https://github.com/AdityaPatil0708/rest-api-service.git">
+              <i class="fab fa-github"></i>
+            </a>
+          </button>
+        </div>
+      </div>
+      <br />
+    </section>
+  );
 }
